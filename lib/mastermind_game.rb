030990e3fb8b1e99baa439b049da0a9b7eb7e4bf
Chris Powell <chris.ian.powell@gmail.com>
2018-04-12 07:17:38 -0600

    game methods complete

--- lib/mastermind_game.rb
+++ lib/mastermind_game.rb
@@ -3,28 +3,26 @@ class Mastermind_game
   attr_reader :template,
               :random_array,
               :test_array_1,
-              :test_array_2,
-              :counter
+              :test_array_2
 
   def initialize
     @template     = ['r','g','b','y']
     @random_array = []
     @test_array_1 = ['r','r','y','g']
-    @test_array_2 = ['r','b','r','g']
+    @test_array_2 = ['r','y','g','b']
     @counter_1    = 0
     @counter_2    = 0
   end
 
-  def random
+  def generate_random_array
     while random_array.length < 4
       random_array << template.sample
     end
-    random_array
   end
 
   def compare_elements
-    test_array_1.each do |color|
-      if test_array_2.include?(color)
+    test_array_2.each do |color|
+      if test_array_1.include?(color)
         @counter_1 += 1
       end
     end
@@ -32,17 +30,12 @@ class Mastermind_game
   end
 
   def compare_positions
-    test_array_1.each do |color|
-      if color = test_array_2[0]
-        @counter_2 += 1
-      elsif color = test_array_2[1]
-        @counter_2 += 1
-      elsif color = test_array_2[2]
-        @counter_2 += 1
-      elsif color = test_array_2[3]
+    test_array_1.each_index do |i|
+      if test_array_1[i] == test_array_2[i]
         @counter_2 += 1
       end
     end
     @counter_2
   end
+
 end
--- test/mastermind_test.rb
+++ test/mastermind_test.rb
@@ -1,7 +1,7 @@
 require './test/test_helper'
 require './lib/mastermind_game'
 
-class Mastermind_GameTest < Minitest::Test
+class Mastermind_SketchTest < Minitest::Test
 
   def test_mastermind_exists
     mastermind = Mastermind_game.new
@@ -18,7 +18,10 @@ class Mastermind_GameTest < Minitest::Test
   def test_generate_random_from_template
     mastermind = Mastermind_game.new
 
-    assert_equal 4, mastermind.random.count
+    mastermind.generate_random_array
+
+    assert_equal 4, mastermind.random_array.length
+    assert mastermind.template != mastermind.random_array
   end
 
   def test_compare_matching_elements
@@ -27,9 +30,9 @@ class Mastermind_GameTest < Minitest::Test
     assert_equal 3, mastermind.compare_elements
   end
 
-  def test_compare_matching_positons
+  def test_compare_positions
     mastermind = Mastermind_game.new
 
-    assert_equal 2, mastermind.compare_positions
+    assert_equal 1, mastermind.compare_positions
   end
 end
